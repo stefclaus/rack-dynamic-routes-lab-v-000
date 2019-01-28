bddf2c4ea465730d150d4bc81f922e85356cc4d7
Stef Claus <stef.claus@gmail.com>
2019-01-28 03:42:12 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,26 +1,25 @@
 class Application
 
-  #@@songs = [Song.new("Sorry", "Justin Bieber"),
-  #          Song.new("Hello","Adele")]
+@@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-  #  if req.path.match(/items/)
-
-  #    item_price = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-  #    items = @@items.find{|s| s.title == item_price}
-
-#      resp.write song.artist
-
-    if req.path=="/items"
-      resp.write "Item price."
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last #turn /item/vase into vase
+      item_price =  @@items.find{|i| i.price == item_price}
+       if item_price != nil
+        resp.write "Item price is #{item.price}"
+        resp.write item.price
+      else
+      resp.write "Item not found"
+      resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
     end
-
     resp.finish
   end
 end
